fda3ee1b7eaefce697e58cb7eab2e2b6469577ce
Kirti Singh <49024860+redkirti@users.noreply.github.com>
2024-08-16 21:49:15 +0530

    Add files via upload

--- Server/models/logs.js
+++ Server/models/logs.js
@@ -1,1 +1,20 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
+const logsSchema = new Schema({
+  website: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true
+  }
+}, { timestamps: true });
+
+const logs = mongoose.model('logs', logsSchema);
+module.exports = logs;
